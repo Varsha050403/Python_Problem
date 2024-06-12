268cdccee3dd09a964a199cf7e779f648d75ea4b
Varsha050403 <112385970+Varsha050403@users.noreply.github.com>
2024-06-12 19:12:36 +0530

    Create 1.Check odd or even.py

---
+++ 1.Check odd or even.py
@@ -0,0 +1,10 @@
+# Write a program to check the number is odd or even.
+def check_even_odd():
+    n = int(input())
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("odd")
+
+
+check_even_odd()
