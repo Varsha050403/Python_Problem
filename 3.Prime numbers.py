da62c23bf53ac3f270657e8d1efc27222c33d885
Varsha050403 <112385970+Varsha050403@users.noreply.github.com>
2024-06-12 19:25:27 +0530

    Create 3.Prime numbers.py

---
+++ 3.Prime numbers.py
@@ -0,0 +1,30 @@
+#Write a python program to find the prime numbers for the given interval
+def prime_numbers():
+    b = int(input())
+
+    for i in range(2, b + 1):
+        if i > 1:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                print(i)
+
+
+prime_numbers()
+
+
+
+lower = 900
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
